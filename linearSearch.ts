ecf2d50d6175e29f1e04c37abcef2bfdbd510711
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-03-01 18:26:33 +0100

    Add linear search

---
+++ linearSearch.ts
@@ -0,0 +1,16 @@
+function linearSearch(array: number[], x: number): boolean {
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === x) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+const a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(linearSearch(a, 6));
+console.log(linearSearch(a, 16));
+
