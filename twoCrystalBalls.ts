9f9ae691094468eb5c96ed7a72aa9e4e20182101
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-03-04 18:20:02 +0100

    Add Two Crystal Balls problem

---
+++ twoCrystalBalls.ts
@@ -0,0 +1,29 @@
+function twoCrystalBalls(breaks: boolean[]): number {
+    let jump = Math.floor(Math.sqrt(breaks.length));
+    let i = jump;
+
+    for (; i < breaks.length; i += jump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jump;
+
+    for (let j = 0; j <= jump && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function testTwoCrystalBalls() {
+    const breaks = [false, false, false, false, true, true, true, true];
+
+    return twoCrystalBalls(breaks);
+}
+
+console.log(testTwoCrystalBalls());
+
